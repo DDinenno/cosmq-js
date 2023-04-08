1e6bb885fc2bd1437b14b66166f8b06d01beb482
domenic D <dom.dinenno@gmail.com>
2023-04-08 09:16:59 -0400

    tweaked index.js to include all the exposed methods

--- dist/index.js
+++ dist/index.js
@@ -1,39 +1,43 @@
-import Observable from "./reactive/Observable"
-import Conditional from "./reactive/Conditional"
-import Formula from "./reactive/Formula"
-import registerComponent from "./registerComponent"
-import {  mountNode, applyProperties, renderElement, renderDOM } from "./DOM"
+import Observable from "./reactive/Observable";
+import Conditional from "./reactive/Conditional";
+import Formula from "./reactive/Formula";
+import core from "./core";
+import { mountNode, applyProperties, renderElement, renderDOM } from "./DOM";
 
-const observe =  (initialValue) => new Observable(initialValue)
-const compute =   (body, deps = []) => new Formula(body, deps)
-const effect =  (body, deps = []) => new Formula(body, deps)
-const conditional = (deps, conditions) => new Conditional(deps, conditions)
+const observe = (initialValue) => new Observable(initialValue);
+const compute = (body, deps = []) => new Formula(body, deps);
+const effect = (body, deps = []) => new Formula(body, deps);
+const conditional = (deps, conditions) => new Conditional(deps, conditions);
 
+const registerComponent = core.registerComponent.bind(core);
+const getComponentContext = core.getComponentContext.bind(core);
+const evalObservable = core.evalObservable.bind(core);
 
+const PlaceholderJs = {
+  observe,
+  compute,
+  effect,
+  conditional,
+  registerComponent,
+  getComponentContext,
+  evalObservable,
+  mountNode,
+  applyProperties,
+  renderElement,
+  renderDOM,
+};
 
-const PlaceHolderJs = {
-    Observable,
- observe,
- compute,
- effect,
- conditional,
- registerComponent,
- mountNode, 
- applyProperties, 
- renderElement, 
- renderDOM
-}
-
-export default PlaceHolderJs 
+export default PlaceholderJs;
 export {
-    observe,
-    compute,
-    effect,
-    conditional,
-    registerComponent,
-    mountNode, 
-    applyProperties, 
-    renderElement, 
-    renderDOM
-}
-  
+  observe,
+  compute,
+  effect,
+  conditional,
+  evalObservable,
+  registerComponent,
+  getComponentContext,
+  mountNode,
+  applyProperties,
+  renderElement,
+  renderDOM,
+};
