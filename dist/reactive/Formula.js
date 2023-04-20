9b80b7adddad35076667065b66f9dfa28ce9fb6f
root <root@DESKTOP-SPS36VU>
2023-04-19 21:00:42 -0400

    simplified Formula class

--- dist/reactive/Formula.js
+++ dist/reactive/Formula.js
@@ -3,33 +3,26 @@ import Observable from "./Observable";
 class Formula extends Observable {
   dependencies = [];
 
-  values = [];
-
-  constructor(generate, deps = []) {
+  constructor(computeFn, dependencies = []) {
     super();
 
-    this.values = this.dependencies.map((d) =>
-      d instanceof Observable ? d.value : d
-    );
-    this.value = generate(...this.values);
+    this.dependencies = dependencies;
 
-    deps.forEach((dep, index) => {
+    this.dependencies.forEach((dep, index) => {
       if (dep instanceof Observable) {
-        this.dependencies[index] = dep;
-
         dep.listen(() => {
-          this.values = [...this.values];
-          this.values[index] = dep.value;
-          this.set(generate(...this.values));
+          this.set(computeFn(...this.dependencies));
         });
-      } else this.dependencies[index] = dep;
+      }
     });
+
+    this.value = computeFn(...this.dependencies);
   }
 
   set(newValue) {
     if (this.value !== newValue) {
       this.value = newValue;
-      this.listeners.forEach((fn) => fn(newValue, this.value));
+      this.listeners.forEach((fn) => fn(newValue));
     }
   }
 }
